fb42afa4738517a2ac850ce1bcbc9fdb84131b51
hkreuter <heike@reuter-consultig.org>
2017-11-03 15:22:45 +0100

    OXDEV-337 Fix typo in file headers

--- metadata.php
+++ metadata.php
@@ -6,7 +6,7 @@
  * @package       tags
  * @author        OXID eSales AG
  * @link          http://www.oxid-esales.com/
- * @copyright (C) OXID eSales AG 2003-20162016
+ * @copyright (C) OXID eSales AG 2003-2016
  */
 
 /**
