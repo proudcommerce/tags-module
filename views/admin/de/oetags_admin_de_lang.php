7731d1af615c9407b72bee7b0502a48f358b64f9
hkreuter <heike@reuter-consultig.org>
2017-11-03 15:23:15 +0100

    OXDEV-337 File encoding must be UTF-8

--- views/admin/de/oetags_admin_de_lang.php
+++ views/admin/de/oetags_admin_de_lang.php
@@ -6,21 +6,21 @@
  * @package       tags
  * @author        OXID eSales AG
  * @link          http://www.oxid-esales.com/
- * @copyright (C) OXID eSales AG 2003-20162016
+ * @copyright (C) OXID eSales AG 2003-2016
  */
 
 
 $sLangName = 'Deutsch';
 
 $aLang = array(
-    'charset'                          => 'ISO-8859-15', // Supports DE chars like: ä, ü, ö, etc.
+    'charset'                          => 'UTF-8',
     'oetags'                           => 'OE Tags',
     'SHOP_MODULE_GROUP_main'           => 'Allgemein',
     'OETAGS_ARTICLE_MAIN_TAGS'         => 'Stichworte',
-    'OETAGS_HELP_ARTICLE_MAIN_TAGS'    => 'Hier können Sie passende Stichworte zum Artikel eingeben. Aus diesen Stichworten wird die Tagcloud (Stichwortwolke) auf der Startseite generiert. Tags werden durch Komma getrennt.',
-    'OETAGS_HELP_SHOP_CONFIG_SHOWTAGS' => 'Wenn nicht angehakt, werden keine Tags im eShop angezeigt. Auf Seiten, die über Tag URL aufgerufen werden könnten, kann nicht zugegriffen werden.',
+    'OETAGS_HELP_ARTICLE_MAIN_TAGS'    => 'Hier kï¿½nnen Sie passende Stichworte zum Artikel eingeben. Aus diesen Stichworten wird die Tagcloud (Stichwortwolke) auf der Startseite generiert. Tags werden durch Komma getrennt.',
+    'OETAGS_HELP_SHOP_CONFIG_SHOWTAGS' => 'Wenn nicht angehakt, werden keine Tags im eShop angezeigt. Auf Seiten, die ï¿½ber Tag URL aufgerufen werden kï¿½nnten, kann nicht zugegriffen werden.',
     'SHOP_MODULE_oetagsShowTags'       => 'Tags im eShop anzeigen',
     'SHOP_MODULE_oetagsSeparator'      => 'Trennzeichen',
     'GENERAL_SEO_TAG'                  => '-- Stichworte --',
-    'ARTICLE_MAIN_INVALIDTAGSFOUND'    => 'Achtung: Unzulässige Tags wurden entfernt.',
+    'ARTICLE_MAIN_INVALIDTAGSFOUND'    => 'Achtung: Unzulï¿½ssige Tags wurden entfernt.',
 );
