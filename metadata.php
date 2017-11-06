c73cfc8c0d0cbed5e11d09cd379d85fad0c0dd76
hkreuter <heike@reuter-consultig.org>
2017-11-06 14:13:42 +0100

    OXDEV-337 Add basket and categories component
    Add basketcomponent and test.
    Add category component test.

--- metadata.php
+++ metadata.php
@@ -43,7 +43,9 @@ $aModule = array(
                            'oxviewconfig' => 'oe/oetags/core/oetagsViewConfig',
                            'oxutilscount' => 'oe/oetags/core/oetagsUtilsCount',
                            'oxlocator' => 'oe/oetags/components/oetagsLocator',
-                           'oxarticle' => 'oe/oetags/models/oetagsArticle'
+                           'oxarticle' => 'oe/oetags/models/oetagsArticle',
+                           'oxcmp_basket' => 'oe/oetags/components/oetagsBasketComponent',
+                           'oxcmp_categories' => 'oe/oetags/components/oetagsCategoriesComponent'
             ),
     'files'       => array(
         'oetagsitaglist' => 'oe/oetags/core/contract/oetagsITagList.php',
@@ -56,7 +58,7 @@ $aModule = array(
         'oetagsArticleTagList' => 'oe/oetags/models/oetagsArticleTagList.php',
         'oetagsTagCloudWidget' => 'oe/oetags/components/widgets/oetagsTagCloudWidget.php',
         'oetagsTagController' => 'oe/oetags/controllers/oetagsTagController.php',
-        'oetagsTagsController' => 'oe/oetags/controllers/oetagsTagsController.php',
+        'oetagsTagsController' => 'oe/oetags/controllers/oetagsTagsController.php'
     ),
     'templates'   => array('widget/sidebar/tags.tpl' => 'oe/oetags/views/widgets/sidebar/tags.tpl',
                            'page/details/inc/tags.tpl' => 'oe/oetags/views/azure/tpl/page/details/inc/tags.tpl',
